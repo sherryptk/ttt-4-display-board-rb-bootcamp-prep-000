abc00df44803d8c69b94908d044f16c8ebbf892b
Sherry <sherryptk@gmail.com>
2017-10-04 21:29:27 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} |   "
+  puts " #{board[0]} | #{board[1]} | #{board[3]} "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
